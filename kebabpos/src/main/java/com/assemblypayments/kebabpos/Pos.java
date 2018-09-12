64d2162d55f2a65a7328e8ccf739ce2816a5d6ac
Metin Avci <mavci@assemblypayments.com>
2018-09-12 16:09:27 +1000

    Increment 2.3.3

--- kebabpos/src/main/java/com/assemblypayments/kebabpos/Pos.java
+++ kebabpos/src/main/java/com/assemblypayments/kebabpos/Pos.java
@@ -49,7 +49,7 @@ public class Pos {
         try {
             // This is how you instantiate SPI while checking for JDK compatibility.
             spi = new Spi(posId, eftposAddress, spiSecrets); // It is ok to not have the secrets yet to start with.
-            spi.setPosInfo("assembly", "2.3.0");
+            spi.setPosInfo("assembly", "2.3.3");
             options = new TransactionOptions();
         } catch (Spi.CompatibilityException e) {
             System.out.println("# ");
