8cebfce7a297141f1fa5ee387d0c863be62cbe9c
Metin Avci <mavci@assemblypayments.com>
2018-11-21 17:01:43 +1100

    RamenPos v2.4.0

---
+++ ramenpos/src/main/java/com/assamblypayments/ramenpos/Enums.java
@@ -0,0 +1,37 @@
+package com.assamblypayments.ramenpos;
+
+public class Enums {
+    static final String Pair = "Pair";
+    static final String UnPair = "UnPair";
+    static final String ConfirmCode = "Confirm Code";
+    static final String OK = "OK";
+    static final String OKUnpaired = "OK-Unpaired";
+    static final String Retry = "Retry";
+    static final String Cancel = "Cancel";
+    static final String CancelPairing = "Cancel Pairing";
+    static final String AcceptSignature = "Accept Signature";
+    static final String DeclineSignature = "Decline Signature";
+    static final String Purchase = "Purchase";
+    static final String Refund = "Refund";
+    static final String CashOut = "Cash Out";
+    static final String MOTO = "MOTO";
+    static final String Recovery = "Recovery";
+    static final String Set = "Set";
+    static final String Retrieve = "Retrieve";
+    static final String Print = "Print";
+    static final String Start = "Start";
+    static final String LastTx = "Get Last Tx";
+
+    static final String Amount = "Amounts (Cents)";
+    static final String TipAmount = "Tip Amount (Cents)";
+    static final String CashoutAmount = "Cashout Amount (Cents)";
+    static final String SurchargeAmount = "Surcharge Amount (Cents)";
+    static final String Reference = "Reference";
+    static final String ReceiptHeader = "Receipt Header";
+    static final String ReceiptFooter = "Receipt Footer";
+    static final String Key = "Key";
+    static final String PrintText = "Print Text";
+
+    static final String PromptForCashout = "Prompt For Cashout";
+    static final String SuppressMerhcantPassword = "Suppress Merhcant Password";
+}
