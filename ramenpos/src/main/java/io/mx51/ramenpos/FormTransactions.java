9a2fb544908d394ad31cadcc1c0fc391077f96a3
Ali Akiltayev <ali@mx51.io>
2021-07-08 17:32:35 +0600

    feature/SP-713: minor fixes

--- ramenpos/src/main/java/io/mx51/ramenpos/FormTransactions.java
+++ ramenpos/src/main/java/io/mx51/ramenpos/FormTransactions.java
@@ -497,6 +497,9 @@ public class FormTransactions implements WindowListener {
         btnRecovery = new JButton();
         btnRecovery.setText("Recovery");
         pnlTransactionalActions.add(btnRecovery, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        btnReversal = new JButton();
+        btnReversal.setText("Reversal");
+        pnlTransactionalActions.add(btnReversal, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         pnlSettings = new JPanel();
         pnlSettings.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 1, new Insets(3, 3, 3, 3), -1, -1));
         pnlMain.add(pnlSettings, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
